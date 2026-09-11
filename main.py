e20da6076c841757c027b5d10aa5076e55c1c28d
piplamitmit <327564770+piplamitmit@users.noreply.github.com>
2026-09-11 18:36:54 +0300

    тест

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 print("main")
 print("123")
+
+print("main")
+print("123")
+
+
 https://pycharm.xeroxsoft.net/?etext=2202.egI0VDAHjCTsSqJyKQn7K2ZsZWloYXB2ZHlpdnFhbHI.3f6f50ad7b94fda4ef4541a4208f72a44f368ab4&yclid=13588264116105248767&ybaip=1
